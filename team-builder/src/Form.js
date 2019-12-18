11e29751b8a0893b24dd85e57822f9c191b03989
Andrea Harris <Andrea@zacs-imac.lan>
2019-12-18 17:15:59 -0600

    working

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -2,5 +2,52 @@ import React, {useState} from "react";
 
 
 const Form = props =>{
-    const [team, setTeams] = useState ({name:' ', email:' ',role: ' '});
+    const [team, setTeam] = useState ({name:' ', email:' ',role: ' '});
+
+    const handleChanges = ele =>{
+        setTeam({...team,[el.target.name]: ele.target.value});
+    };
+
+    const handleSubmit = ele => {
+        el.preventDefault();
+        props.form (team);
+        setTeam ({name:' ', email: ' ', role: ' '});
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
